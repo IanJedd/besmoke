0596d1add02f62cba90a8e5b8f8ec22be2b9438b
IanJedd <i.am.jedd5.1@gmail.com>
2017-10-25 14:38:20 -0600

    Further work on password class

--- src/Password.java
+++ src/Password.java
@@ -1,12 +1,21 @@
-import java.security.*
+import java.security.*;
 
 public class Password {
+	public static final int SALTYSIZE = 32; //in bytes
 	private String saltedHashedPassword;
 	private String salt;
 	private SecureRandom random;
 
+	public static void main(String args[]) {
+		Password passy = new Password("ayy lmao");
+	}
+
 	public Password(String initPassword) {
-		
+		random = new SecureRandom();
+		byte[] saltyBytes = new byte[SALTYSIZE];
+		random.nextBytes(saltyBytes);
+		for (int i = 0; i < SALTYSIZE; i++) {salt += (char) saltyBytes[i];}
+		System.out.println(salt);
 	}
 
 	public void updateHashedPassword(String newPassword) {
@@ -14,6 +23,7 @@ public class Password {
 	}
 
 	public boolean checkPassword(String questionablePassword) {
-
+		//placeholder
+		return false;
 	}
 }
