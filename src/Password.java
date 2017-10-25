eb15e3e3f957b4d36478c274008eb8d21e9cf205
IanJedd <i.am.jedd5.1@gmail.com>
2017-10-25 14:12:29 -0600

    Worked on Password class

---
+++ src/Password.java
@@ -0,0 +1,19 @@
+import java.security.*
+
+public class Password {
+	private String saltedHashedPassword;
+	private String salt;
+	private SecureRandom random;
+
+	public Password(String initPassword) {
+		
+	}
+
+	public void updateHashedPassword(String newPassword) {
+
+	}
+
+	public boolean checkPassword(String questionablePassword) {
+
+	}
+}
