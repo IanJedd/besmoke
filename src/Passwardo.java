ebb12bb1890b0fff561501be9ff9c755711c8c88
IanJedd <i.am.jedd5.1@gmail.com>
2017-11-13 16:45:05 -0700

    fixed some password errors

--- src/Passwardo.java
+++ src/Passwardo.java
@@ -2,14 +2,14 @@ package besmoke.src;
 import java.security.*;
 import javax.crypto.*;
 import java.util.*;
+import java.io.Serializable;
 
-public class Passwardo {
+public class Passwardo implements Serializable{
 	//please don't read my code.
 	public static final int SALTYSIZE = 32; //in bytes
 	private String saltedHashedPassword;
 	private String salt;
 	private SecureRandom random;
-	private MessageDigest sexyMessageDigest;
 
 	public static void main(String args[]) {
 		Passwardo testy = new Passwardo("ayy lmao");
@@ -24,13 +24,14 @@ public class Passwardo {
 		byte[] saltyBytes = new byte[SALTYSIZE];
 		random.nextBytes(saltyBytes);
 		for (int i = 0; i < SALTYSIZE; i++) {salt += (char) saltyBytes[i];}
-	 	try {sexyMessageDigest = MessageDigest.getInstance("SHA-256");}
-		catch (Exception e) {System.out.println("Did you ever find Bugs Bunny attractive when he put on a dress and played a girl bunny? Neither did I");}
 		updateHashedPassword(initPassword);
 		initPassword = "What are you trying to do, hack me? We don't take kindly to hackers round these parts.";
 	}
 
 	public void updateHashedPassword(String newPassword) {
+		MessageDigest sexyMessageDigest = null;
+		try {sexyMessageDigest = MessageDigest.getInstance("SHA-256");}
+		catch (Exception e) {System.out.println("Did you ever find Bugs Bunny attractive when he put on a dress and played a girl bunny? Neither did I");}
 		byte[] saltedHashyBytes = sexyMessageDigest.digest((newPassword + salt).getBytes());
 		newPassword = "What are you trying to do, hack me? We don't take kindly to hackers round these parts.";
 		saltedHashedPassword = "";
@@ -42,6 +43,9 @@ public class Passwardo {
 	}
 
 	public boolean checkPassword(String questionablePassword) {
+		MessageDigest sexyMessageDigest = null;
+		try {sexyMessageDigest = MessageDigest.getInstance("SHA-256");}
+		catch (Exception e) {System.out.println("Did you ever find Bugs Bunny attractive when he put on a dress and played a girl bunny? Neither did I");}
 		String questionableSaltedHashedPassword = "";
 		byte[] testSaltedHashyBytes = sexyMessageDigest.digest((questionablePassword + salt).getBytes());
 		for (int i = 0; i < testSaltedHashyBytes.length; i++) {questionableSaltedHashedPassword += (char) testSaltedHashyBytes[i];}
