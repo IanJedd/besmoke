9872a10e9963eba050e4c341f659e9bf56d65e52
Matthew Kelly <31663023+mattkellymt@users.noreply.github.com>
2017-12-12 20:37:19 -0700

    commenting!

--- Version3/BeFinanced/src/befinanced/Model.java
+++ Version3/BeFinanced/src/befinanced/Model.java
@@ -7,7 +7,16 @@ import java.util.HashMap;
 public class Model {
     static Stage window;
 
-    static HashMap<String, Object> table = new HashMap<>();
+    //Three Tables for the Elven-kings under the sky,
+    //Seven for the Dwarf-lords in their halls of stone,
+    //Nine for Mortal Men doomed to die,
+    //One for the Dark Lord on his dark throne
+    //In the Land of Mordor where the Shadows lie.
+    //One Table to rule them all, One Table to find them,
+    //One Table to bring them all, and in the darkness bind them,
+    //In the Land of Mordor where the Shadows lie.
+    static HashMap<String, Object> table;
+    //Users and user are loaded 
     static HashMap<String, Object> users;
     static HashMap<String, Object> user;
     static HashMap<String, Object> accounts;
@@ -19,11 +28,15 @@ public class Model {
     static HashMap<String, Integer> savedDimensions;
 
     static String title = "BeFinanced by BeSmoke";
+    static String tableDirectory = "./befinanced/Data/";
+    static String tableFilename = "BeTheTable.table";
+    static String tablePath = tableDirectory + tableFilename;
     static String imageDirectory = "./befinanced/Images/";
     static String logo = "BeFinanced.png";
     static String fxmlDirectory = "./FXML/";
     static String fxmlExtension = ".fxml";
     static String fxmlFilename;
+    // savedFXMLFilename lets the back function know what file to load
     static String savedFXMLFilename;
     static String username;
     static String saltedHashedPassword;
@@ -40,6 +53,9 @@ public class Model {
     static int savedWidth;
 
     static {
+        //We need to make this method. Maybe serialize the object like Erik did.
+        loadTable();
+
         int littleHeight = 200;
         int littleWidth = 300;
         int bigHeight = 400;
@@ -62,9 +78,16 @@ public class Model {
         users = new HashMap<>();
         table.put("Users", users);
     }
+    public static void loadTable()
+    {
+        //A method for loading a table with the filename Model.tableFilename
+        boolean success = false;
+        if (!success) { table = new HashMap<>();}
+    }
 
-    public static Object value(String key) {
-        return table.get(key);
+    public static void saveTable()
+    {
+        //A method for saving the table. Is serializing a Java object a thing? How'd you do that Erik?
     }
 
     public static void main(String[] args) {}
